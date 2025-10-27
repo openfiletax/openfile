7c250d8d6ea39819189245b81f489099e355ff74
Elijah Wright <git@elijahs.space>
2025-10-27 10:38:36 -0700

    fix FactConfig

--- direct-file/backend/src/main/java/gov/irs/directfile/api/factgraph/FactConfig.java
+++ direct-file/backend/src/main/java/gov/irs/directfile/api/factgraph/FactConfig.java
@@ -8,17 +8,23 @@ public class FactConfig implements gov.irs.factgraph.definitions.fact.FactConfig
             String path,
             gov.irs.factgraph.definitions.fact.WritableConfigTrait writable,
             gov.irs.factgraph.definitions.fact.CompNodeConfigTrait derived,
-            gov.irs.factgraph.definitions.fact.CompNodeConfigTrait placeholder) {
+            gov.irs.factgraph.definitions.fact.CompNodeConfigTrait placeholder,
+            gov.irs.factgraph.definitions.fact.CompNodeConfigTrait overrideCondition,
+            gov.irs.factgraph.definitions.fact.CompNodeConfigTrait overrideDefault) {
         this.path = path;
         this.writable = writable;
         this.derived = derived;
         this.placeholder = placeholder;
+        this.overrideCondition = overrideCondition;
+        this.overrideDefault = overrideDefault;
     }
 
     public String path;
     public gov.irs.factgraph.definitions.fact.WritableConfigTrait writable;
     public gov.irs.factgraph.definitions.fact.CompNodeConfigTrait derived;
     public gov.irs.factgraph.definitions.fact.CompNodeConfigTrait placeholder;
+    public gov.irs.factgraph.definitions.fact.CompNodeConfigTrait overrideCondition;
+    public gov.irs.factgraph.definitions.fact.CompNodeConfigTrait overrideDefault;
 
     @Override
     public String path() {
